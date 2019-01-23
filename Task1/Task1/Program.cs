b95fa8759ab157f762a51a2c4b679841e0937c6c
Eliot Ostling <eostling@cnm.edu>
2019-01-22 17:43:03 -0700

    Graded: Pass

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,6 +6,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //Good
             //setup
             Single milesDriven = 0;
             Single gallonsUsed = 0;
